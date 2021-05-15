996a7f887b3eed1266386276512c4f002cdb171b
mpoziomska <mpoziomska@wp.pl>
2021-05-15 15:23:14 +0200

    New

--- scripts/positive_rate.py
+++ scripts/positive_rate.py
@@ -2,7 +2,9 @@ from utils import *
 
 line_plot_interactive(col = 'owid-covid-data_positive_rate', 
                       countries = ['Denmark', 'United Kingdom', 'South Korea', 'Switzerland'], 
-                      title = "Positive rate", ylabel='Positive rate', th = False, 
+                      title = "Positive rate", 
+                      ylabel='Positive rate', 
+                      th = False, 
                       file='positive_rate_interactive', 
                       legloc='top_right')
 
